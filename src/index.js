96b6e3b13c67fce029dae7cdd974ef41a3c1e278
Matthew <turbodieseldodge@gmail.com>
2022-12-20 10:43:57 -0600

    good progress

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-fetch ('http://localhost:3000/movies')
-.then ((response) => response.json())
-.then ((movies) => {
-    movies.forEach(movie => {
-        showMovieCover(movie)
-    })
-})
+fetch(`http://localhost:3000/movies`)
+    .then((response) => response.json())
+    .then((movies) => {
+        console.log(movies)
+        movies.forEach(renderMovie)
+        movies.forEach(displayInfo)
+    });
+    setdisplayInfo(movies)[0]
 
-
-function showMovieCover(movie) {
-    console.log('showMovieCover', movie)
-    const nav = document.getElementById('movie-list')
-    const img = document.getElementById('img')
-
-    // console.log(img)
-    console.log(nav)
-
-
+function renderMovie(movie) {
+    const img = document.createElement('img');
+    img.src = movie.image;
+    //li.textContent = "hi"
+    document.querySelector('#movie-list').append(img)
 }
 
+function displayInfo(movie) {
+    const movieInfo = document.querySelector('#title')
+    const yearReleased = document.querySelector('#year-released')
+    const description = document.querySelector('#description')
+    const detailImage = document.querySelector('#detail-image')
+    movieInfo.innerHTML = movie.title
+    yearReleased.innerHTML = movie.release_year
+    description.innerHTML = movie.description
+    detailImage.src = movie.image
+
+}
+// displayInfo()
